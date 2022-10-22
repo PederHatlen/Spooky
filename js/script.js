0466bf8cb0deb04ee78cb16d28e0355bacb732ab
Peder Hatlen <pehaa002@osloskolen.no>
2022-10-22 16:14:56 +0200

    new prompts

--- js/script.js
+++ js/script.js
@@ -2,16 +2,19 @@ let spookerEl = document.getElementById("spooker");
 let spookMoreEl = document.getElementById("more");
 
 let spooks = [
-	"Ammerican politics.",
-	"Infalation.",
+	"American politics.",
+	"Inflation.",
 	"UK politics.",
 	"Global warming.",
-	"Ocean temperature.",
+	"Ocean temperatures.",
 	"Energy prices.",
 	"Solar flares.",
 	"Ghosts.",
 	"Ghouls?",
-	"Nuclear threts.",
+	"nuclear threats.",
+	"<a href=\"https://youtu.be/-1dSY6ZuXEY\">Skeletons.</a>",
+	"<a href=\"https://youtu.be/dQw4w9WgXcQ\">The internet.</a>",
+	"The spanish inquisition (not expected)",
 	"Boo."
 ];
 
